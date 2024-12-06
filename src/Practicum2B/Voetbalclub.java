1fc173af7c445e4983f86e6c44ecf0da96789120
Pieter Verweij <pieter.verweij@pegamento.nl>
2024-12-06 01:12:41 +0100

    practicum 2b

---
+++ src/Practicum2B/Voetbalclub.java
@@ -0,0 +1,55 @@
+package Practicum2B;
+
+public class Voetbalclub {
+    private String naam;
+    private int aantalGewonnen;
+    private int aantalGelijk;
+    private int aantalVerloren;
+
+    // Constructor
+    public Voetbalclub(String naam) {
+        this.naam = naam.trim(); // Trim om overbodige spaties te verwijderen
+        this.aantalGewonnen = 0;
+        this.aantalGelijk = 0;
+        this.aantalVerloren = 0;
+    }
+
+    // Methode om een resultaat te verwerken
+    public void verwerkResultaat(char resultaat) {
+        switch (resultaat) {
+            case 'w': // gewonnen
+                aantalGewonnen++;
+                break;
+            case 'g': // gelijkspel
+                aantalGelijk++;
+                break;
+            case 'v': // verloren
+                aantalVerloren++;
+                break;
+            default:
+                System.out.println("Ongeldig resultaat: " + resultaat);
+        }
+    }
+
+    // Methode om totaal aantal punten te berekenen
+    public int aantalPunten() {
+        return (aantalGewonnen * 3) + (aantalGelijk);
+    }
+
+    // Methode om totaal aantal gespeelde wedstrijden te berekenen
+    public int aantalGespeeld() {
+        return aantalGewonnen + aantalGelijk + aantalVerloren;
+    }
+
+    // toString methode
+    @Override
+    public String toString() {
+        return naam + "\t" +
+                aantalGespeeld() + " " +
+                aantalGewonnen + " " +
+                aantalGelijk + " " +
+                aantalVerloren + " " +
+                aantalPunten() + " ";
+    }
+}
+
